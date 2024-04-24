c05a9003bebf303878a77ffb197b3379625bfc73
Roman Ivanov <sitdoff@gmail.com>
2024-04-24 08:54:22 +0300

    update EmaiUsernameAuthenticationBackend

--- freelance/users/backends.py
+++ freelance/users/backends.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
 from django.db.models import Q
 
-UserModel = get_user_model()
+from .models import UserModel
 
 
 class EmailUsernameAuthenticationBackend(ModelBackend):
