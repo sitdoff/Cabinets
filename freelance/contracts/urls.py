1b644451ead575c1abd9b642d43669abfab1afc3
Roman Ivanov <sitdoff@gmail.com>
2024-04-25 18:03:35 +0300

    add contract_detail

--- freelance/contracts/urls.py
+++ freelance/contracts/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.Index.as_view(), name="index"),
+    path("contract/detail/<int:pk>/", views.ContractDetailView.as_view(), name="contract_detail"),
 ]
--- freelance/contracts/views.py
+++ freelance/contracts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 
 from .models import ContractModel
 
@@ -10,3 +11,9 @@ class Index(ListView):
     queryset = ContractModel.objects.all()
     template_name = "index.html"
     context_object_name = "contract_list"
+
+
+class ContractDetailView(DetailView):
+    model = ContractModel
+    template_name = "contracts/contract_detail.html"
+    context_object_name = "contract"
