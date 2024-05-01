909acaafc1765ca2ebb3b4d5b576d8bbf68f07cf
Roman Ivanov <sitdoff@gmail.com>
2024-05-01 08:51:26 +0300

    add offer admin model

--- freelance/offer/admin.py
+++ freelance/offer/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import OfferModel
+
+
 # Register your models here.
+class OfferModelAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(OfferModel, OfferModelAdmin)
--- freelance/offer/models.py
+++ freelance/offer/models.py
@@ -9,3 +9,9 @@ class OfferModel(models.Model):
     offering = models.ForeignKey(UserModel, on_delete=models.CASCADE, related_name="user_offers")
     contract = models.ForeignKey(ContractModel, on_delete=models.CASCADE, related_name="contract_offers")
     message = models.TextField(max_length=500, blank=True, verbose_name="Сопроводительное письмо")
+
+    class Meta:
+        verbose_name_plural = "Offers"
+
+    def __str__(self) -> str:
+        return f"{self.offering} offer {self.contract}"
