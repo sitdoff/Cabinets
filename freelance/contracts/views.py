6a90c2012f3c0ee0b222ab2535d98348c5590abb
Roman Ivanov <sitdoff@gmail.com>
2024-05-02 12:09:21 +0300

    update template contract_detail.html

--- freelance/contracts/views.py
+++ freelance/contracts/views.py
@@ -28,6 +28,13 @@ class ContractDetailView(DetailView):
     template_name = "contracts/contract_detail.html"
     context_object_name = "contract"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["is_offered"] = (
+            context[self.context_object_name].contract_offers.filter(offering=self.request.user).exists()
+        )
+        return context
+
 
 class CreateContractView(CreateView):
     """
