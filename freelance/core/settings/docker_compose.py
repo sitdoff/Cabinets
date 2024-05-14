f3ba8094c8ed1d27c6d36b8484264cd406c33605
Roman Ivanov <sitdoff@gmail.com>
2024-05-14 12:55:16 +0300

    update settings/docker_compose.py

--- freelance/core/settings/docker_compose.py
+++ freelance/core/settings/docker_compose.py
@@ -1,5 +1,9 @@
 from core.settings.base import *
 
+print("-" * 50)
+print("WARNING! DOCKER COMPOSE SETTINGS!")
+print("-" * 50)
+
 # For django_gebug_toolbar
 if DEBUG:
     import socket  # only if you haven't already imported this
